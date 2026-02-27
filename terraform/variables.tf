94abd9124b6661720341f9982945aabdf162f60e
Ruben J Valdez Escobedo <44850914+youkaimx@users.noreply.github.com>
2026-02-26 19:24:40 -0600

    Add dynamic credentials for TFC

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -27,3 +27,15 @@ variable "s3_vectors_bucket_name" {
   type        = string
   default     = ""
 }
+
+variable "tfc_aws_dynamic_credentials" {
+  description = "Object containing AWS dynamic credentials configuration"
+  type = object({
+    default = object({
+      shared_config_file = string
+    })
+    aliases = map(object({
+      shared_config_file = string
+    }))
+  })
+}
